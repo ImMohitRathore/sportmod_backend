fe24b0577e0fb8072c6b8f0b53fea8d70a27a596
weindic <weindic.inc@gmail.com>
2024-11-19 01:56:55 +0530

    df

--- index.js
+++ index.js
@@ -17,12 +17,16 @@ app.use(cors(corsOptions));
 // Configure multer for file uploads
 const upload = multer();
 app.use(upload.any());
-// 
 
 // Middleware for parsing request bodies
 app.use(bodyParser.text({ type: "/" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// dashboard Routes
+app.use("/admin", require("./route/admin.route.js")); // All user-related APIs
+
+
 
 // Grouped API Routes
 app.use("/user", require("./route/User.route.js")); // All user-related APIs
@@ -32,8 +36,23 @@ app.use("/team", require("./route/team.route")); // All team-related APIs
 app.use("/game", require("./route/game.route")); // All game-related APIs
 app.use("/ground", require("./route/ground.route")); // All ground-related APIs
 
+// Route to serve the welcome message
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Welcome to Conqore</title>
+      </head>
+      <body>
+        <h1>Welcome to Conqore!</h1>
+        <p>Your app is up and running!</p>
+      </body>
+    </html>
+  `);
+});
+
 // Server setup
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
 });
