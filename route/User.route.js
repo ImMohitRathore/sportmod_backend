860e702887ac58893bf964fe101a5835fe924cd6
imMohitRathore <hacksidee715@gmail.com>
2024-11-20 01:26:51 +0530

    fixed

--- route/User.route.js
+++ route/User.route.js
@@ -7,8 +7,15 @@ Router.put("/userLogin", controller.userLogin);
 Router.put("/userDataSave", controller.UserDataSave);
 Router.put("/add", Service.addData);
 Router.get("/profile", authenticateJWT, Service.profileData);
+Router.get("/UserDetail/:id", authenticateJWT, Service.UserDetail);
+
 Router.get("/getusers", authenticateJWT, Service.getUsers);
 Router.get("/getFollowerList", authenticateJWT, Service.getFollowerList);
+Router.get(
+  "/getFollowerList_other_user/:id",
+  authenticateJWT,
+  Service.getFollowerListOtherUser
+);
 Router.post("/sendOtp", controller.sendotp);
 Router.post("/otpverify", controller.otpverify);
 Router.put("/updatePassword", controller.updatePassword);
